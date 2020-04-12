547b77a19b18de03840e16bdad66f8662118c5af
ygluogit <ygluo@yahoo.com>
2020-04-12 13:03:21 -0400

    Add files via upload

---
+++ TestMain.java
@@ -0,0 +1,24 @@
+package com.java8;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+
+public class TestMain {
+
+    public static void main(String[] args) {
+
+
+    	HashSet<String> set=new HashSet<String>(); 
+    	System.out.println("size: " + set.size());
+        set.add("Ravi"); 
+        System.out.println("size: " + set.size());
+        if (!set.add("Ravi")) System.out.println("size not change. " + set.size());  
+    	
+      }
+
+}
+    
+   
